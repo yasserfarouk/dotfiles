d01f9b14b52041a14a8697a5d142372597e2de1e
Yasser Mohammad <yasser@yasser.local>
2020-03-03 10:43:54 +0900

    do not delete z history

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ rm -rf $HOME/.dotfiles
 rm -rf $HOME/.vim
 rm -rf $HOME/.tmux
 rm -rf $HOME/antigen
-rm -rf $HOME/.z
+# rm -rf $HOME/.z
 rm $HOME/.vimrc
 rm $HOME/.bashrc
 rm $HOME/.zshrc
 rm $HOME/.tmux.conf
-rm -rf $HOME/.z-data
+# rm -rf $HOME/.z-data
 
 ./install.sh
