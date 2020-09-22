9803f8f0b826042ab3df9907cee91251db2460ff
Yasser Mohammad <yasserfarouk@gmail.com>
2020-09-22 22:01:29 +0900

    adding bootstrap files

---
+++ install.sh
@@ -0,0 +1,337 @@
+#!/usr/bin/env bash
+
+while true; do
+	read -p "Do you have root access to this machine? [yn] " has_root
+	case $has_root in
+		[Yy]* ) echo "May be prompted for your password later"; break;;
+		[Nn]* ) echo "Will not be prompted for a sudo password"; break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
+
+packages=(
+	"stow"
+	"ccls"
+	"gdb"
+	"git"
+	"node"
+	"neovim"
+	"zsh"
+	"fzf"
+	"fd"
+	"vifm"
+	"tree"
+	"pyenv-virtualenv"
+	"ag"
+	"ripgrep"
+	"languagetool"
+	"antiword"
+	"docx2txt"
+	"poppler"
+	"mutt"
+	"yarn"
+	"ack"
+	"bat"
+	"cloc"
+	"diff-so-fancy"
+	"entr"
+	"exa"
+	"gh"
+	"gnupg"
+	"grep"
+	"highlight"
+	"htop"
+	"hub"
+	"jq"
+	"lazydocker"
+	"lazygit"
+	"markdown"
+	"mas"
+	"neofetch"
+	"nmap"
+	"noti"
+	"reattach-to-user-namespace"
+	"schniz/tap/fnm"
+	"shellcheck"
+	"tig"
+	"tldr"
+	"tmux"
+	"trash"
+	"tree"
+	"vim"
+	"wdiff"
+	"wget"
+	"youtube-dl"
+	"z"
+	"zsh"
+	"neofetch"
+	"zathura"
+	"zathura-pdf-poppler"
+	"transmission"
+)
+pip_packages=(
+	"msgpack"
+)
+linux_packages=(
+	"terminal-notifier"
+)
+
+mac_packages=(
+	"alacritty"
+	"macvim"
+	"terminal-notifier"
+	"mackup"
+)
+
+python_packages=(
+	"neovim"
+	"flake8"
+)
+case "$(uname -s)" in
+	Darwin)
+		brew="/usr/local/bin/brew"
+		if [ -f "$brew" ]
+		then
+			echo "Homebrew is installed, nothing to do here"
+		else
+			echo "Homebrew is not installed, installing now"
+			echo "This may take a while"
+			echo "Homebrew requires osx command lines tools, please download xcode first"
+			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+		fi
+
+		;;
+
+	Linux)
+		[[ -e "$HOME/bin" ]] || mkdir "$HOME/bin"
+
+	# curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage ~/bin
+	# chmod u+x ~/bin/nvim.appimage
+	# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	# echo "neovim is installed in ~/bin/nvim.appimage. alias it to vim after installation"
+	# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	case $has_root in
+		[Yy]* ) sudo apt install make lib64readline7 lib64readline7-dev sqlite sqlite3 zlib1g-dev zlibig bzip2;;
+		[Nn]* ) echo "You should have make lib64readline7 lib64readline7-dev sqlite sqlite3 zlib1g-dev zlibig bzip2 installed. Proceeding assuming you do";;
+		* ) echo "Should not be here";;
+	esac
+	command -v brew 2>&1 >/dev/null # improvement by tripleee
+	BREW_IS_AVAILABLE=$?
+	if [ $BREW_IS_AVAILABLE ]
+	then
+		echo "Homebrew is installed, nothing to do here"
+	else
+		echo "Installing Linuxbrew"
+		echo "---------------------"
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+		test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+		export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
+		echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bashrc
+	fi
+	brew install gcc
+	brew upgrade gcc
+	brew unlink gcc && brew link gcc
+
+	;;
+
+esac
+
+
+brew tap "homebrew/bundle"
+brew tap "homebrew/cask"
+brew tap "homebrew/core"
+brew tap "homebrew/cask-fonts"
+brew tap "schniz/tap"
+
+for i in "${packages[@]}"
+do
+	#brew unlink $i
+	#brew uninstall $i
+	brew install $i
+	brew upgrade $i
+	brew unlink $i && brew link $i
+	echo "---------------------------------------------------------"
+done
+
+case "$(uname -s)" in
+	Darwin)
+		for i in "${mac_packages[@]}"
+		do
+			#brew unlink $i
+			#brew uninstall $i
+			brew install $i
+			brew upgrade $i
+			brew unlink $i && brew link $i
+			echo "---------------------------------------------------------"
+		done
+
+		;;
+
+	Linux)
+		for i in "${linux_packages[@]}"
+		do
+			#brew unlink $i
+			#brew uninstall $i
+			brew install $i
+			brew upgrade $i
+			brew unlink $i && brew link $i
+			echo "---------------------------------------------------------"
+		done
+		brew install vim --HEAD --without-python --with-python3
+		brew unlink vim
+		brew link vim
+		;;
+esac
+
+
+# installing fzf shortcuts
+yes | $(brew --prefix)/opt/fzf/install --all
+
+# installing programs that require special care (e.g. tmux from HEAD and devicons usin npm)
+brew install tmux --HEAD
+brew link tmux
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+brew cask link font-hack-nerd-font
+current_dir=`pwd`
+cd $HOME
+npm install -g devicons
+npm install -g write-good
+cd $current_dir
+
+command -v git 2>&1 >/dev/null # improvement by tripleee
+GIT_IS_AVAILABLE=$?
+
+if [ $GIT_IS_AVAILABLE ]
+then
+	echo "git is all good"
+else
+	brew install git
+	brew upgrad git
+fi
+# Okay so everything should be good
+# Now lets clone my dotfiles repo into .dotfiles/
+
+echo "Installing pip packages assuming that pip is available"
+echo "------------------------------------------------------"
+for i in "${pip_packages[@]}"
+do
+	pip3 install --upgrade $i
+done
+
+echo "Installing neovim2/3 python envs"
+echo "--------------------------------"
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install 2.7.17
+# pyenv install 3.7.6
+pyenv install 3.8.1
+
+pyenv virtualenv 2.7.17 neovim2 2>&1 >/dev/null
+# pyenv virtualenv 3.7.6 neovim3 2>&1 >/dev/null
+pyenv virtualenv 3.8.1 neovim3 2>&1 >/dev/null
+
+echo "Installing neovim for python, and node"
+echo "--------------------------------------------"
+pyenv activate neovim2
+pip install --upgrade pip
+for i in "${python_packages[@]}"
+do
+	pip install $i
+done
+
+neovim2_py=`pyenv which python`  # Note the path
+
+# pyenv activate neovim3
+# pip install --upgrade pip
+# pip install neovim-remote
+# for i in "${python_packages[@]}"
+# do
+# pip install $i
+# done
+# neovim3_py=`pyenv which python`  # Note the path
+
+pyenv activate neovim3
+pip install --upgrade pip
+pip install neovim-remote
+
+for i in "${python_packages[@]}"
+do
+	pip install -U $i
+done
+neovim3_py=`pyenv which python`  # Note the path
+
+gem install neovim
+npm install -g neovim
+echo "Installing CTAGS"
+echo "----------------"
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+
+echo "Installing diff-so-fancy"
+echo "------------------------"
+mkdir ~/bin 2>&1 >/dev/null
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy >~/bin/diff-so-fancy
+chmod +x ~/bin/diff-so-fancy
+
+echo "Installing source code nerd font"
+echo "--------------------------------"
+$current_dir=`pwd`
+mkdir $HOME/fonts 2>&1 >/dev/null
+cd $HOME/fonts
+curl -OJL https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/SourceCodePro.zip
+unzip SourceCodePro.zip
+
+curl -OJL https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/FiraCode.zip
+unzip FiraCode.zip
+
+rm "*windows*" 2>&1 >/dev/null
+rm "*Windows*" 2>&1 >/dev/null
+case "$(uname -s)" in
+	Darwin)
+		mkdir $HOME/Library/Fonts 2>&1 >/dev/null
+		mv *.ttf $HOME/Library/Fonts
+		;;
+
+	Linux)
+		mkdir $HOME/.fonts 2>&1 >/dev/null
+		mv *.ttf $HOME/.fonts
+		;;
+esac
+cd $current_dir
+
+echo "Linking Zathura PDF"
+mkdir -p $(brew --prefix zathura)/lib/zathura
+ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+
+echo "Installing Plug for NVIM"
+echo "-------------------------"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing antigen for zsh"
+echo "--------------------------"
+mkdir ~/antigen 2>&1 >/dev/null
+curl -L git.io/antigen > ~/antigen/antigen.zsh
+
+echo "Installing pure theme"
+echo "--------------------------"
+npm install --global pure-prompt
+
+echo "Updating npm"
+echo "------------"
+npm install -g npm
+
+echo "----------------------"
+echo "Installed prerequisits"
+echo "----------------------"
+./quick-install.sh
+
+
+echo "-----------------------------"
+echo "           Manual Installs   "
+echo "-----------------------------"
+echo "- Install MacVim latest release from https://github.com/macvim-dev/macvim/releases/tag/snapshot-147"
---
+++ quick-install.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env zsh
+echo "Updating init files with replacements"
+echo "-------------------------------------"
+export PATH="~/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv activate neovim2
+neovim2_py=`pyenv which python`  # Note the path
+echo "neovim2 in $neovim2_py"
+pyenv activate neovim3
+neovim3_py=`pyenv which python`  # Note the path
+echo "neovim3 in $neovim3_py"
+
+function replace_tag_in_file(){
+  python -c "s=open('$3', 'r').read().replace('$1','$2'); open('$3', 'w').write(s)" 2>&1 >/dev/null
+}
+
+# function replace_tag_in_all(){
+#   for file_name in $(find ~/.dotfiles -type f -and ! -name '*.otf' -and ! -name '.*' -and ! -path '*tmux/plugins*' -and ! -name '*.png' -and ! -name 'Makefile' -and ! -path '*z*' );
+#   do
+#     python -c "s=open('$file_name', 'r').read().replace('$1','$2'); open('$file_name', 'w').write(s)" 2>&1 >/dev/null
+#   done
+#   for file_name in $(find ~/.ysupport -type f -and ! -name '*.otf' -and ! -name '.*' -and ! -path '*tmux/plugins*' -and ! -name 'Makefile' -and ! -name '*.png' -and ! -path '*z*');
+#   do
+#     python -c "s=open('$file_name', 'r').read().replace('$1','$2'); open('$file_name', 'w').write(s)"  2>&1 >/dev/null
+#   done
+# }
+#
+# replace_tag_in_file '<<nvimpy2>>' $neovim2_py "$HOME/.config/nvim/init.vim"
+# replace_tag_in_file '<<nvimpy3>>' $neovim3_py "$HOME/.config/nvim/init.vim"
+#
+#
+# cd $HOME
+# echo "running RCM rcup command"
+# echo "This is symlink the rc files"
+#
+# case "$(uname -s)" in
+#    Darwin)
+#     rcup
+#     ;;
+#    Linux)
+#     rcup
+#     export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
+#     echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bashrc
+#     ;;
+# esac
+# echo "---------------------------------------------------------"
+
+echo "Sourcing all files"
+echo "------------------"
+mkdir $HOME/.vim/doc 2>&1 >/dev/null
+#vim --cmd "helptags $HOME/.vim/doc" --cmd "q"
+source ~/.bash_profile
+source ~/.zshrc
+echo "Installing Plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing vim plugins"
+echo "----------------------"
+vim +PlugInstall +UpdateRemotePlugins +qa
+nvim +PlugInstall +UpdateRemotePlugins +CheckHealth +qa
+
+echo "Changing to zsh"
+chsh -s $(which zsh)
+
+
+echo "You will need to log out for this to take effect"
+echo "----------------------------------------------"
+
+
+case "$(uname -s)" in
+   Darwin)
+     echo "running oxs defaults"
+     ./osx.sh
+
+     ;;
+
+   Linux)
+     echo 'Linux ... no osx defaults'
+
+     ;;
+esac
+
+echo "Correcting group permissions"
+echo "----------------------------"
+compaudit | xargs chmod g-w
+
+./qupdate
+
+echo "------------------------------------------------------------"
+echo "                             All done!                      "
+echo "Change your terminal font to <Source Code Pro for Powerline>"
+echo "------------------------------------------------------------"
+exit 0
