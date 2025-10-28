c3557b44ddc93e555c6eca9fe60ec169597eb458
Yasser Mohammad <yasserfarouk@gmail.com>
2025-10-28 14:25:24 +0100

    fix(nvim): migrate to vim.lsp.config API for Neovim 0.11
    Replace deprecated require('lspconfig')[server].setup() with vim.lsp.config()
    and vim.lsp.enable() as per Neovim 0.11 LSP configuration changes.

--- base/nvim/.config/nvim/lua/plugins/lsp/servers.lua
+++ base/nvim/.config/nvim/lua/plugins/lsp/servers.lua
@@ -45,20 +45,21 @@ function M.setup(_, opts)
 	-- Get default capabilities
 	local capabilities = lsp_utils.capabilities()
 
-	-- Setup mason-lspconfig to install and configure servers
+	-- Setup mason-lspconfig to install servers
 	local servers = opts.servers or {}
 	require("mason-lspconfig").setup({
 		ensure_installed = vim.tbl_keys(servers),
 		automatic_enable = false,
 	})
 
-	-- Setup each server with its configuration
+	-- Configure each server using vim.lsp.config
 	for server_name, server_config in pairs(servers) do
-		local server_opts = vim.tbl_deep_extend("force", {
+		local config = vim.tbl_deep_extend("force", {
 			capabilities = capabilities,
 		}, server_config)
 		
-		require("lspconfig")[server_name].setup(server_opts)
+		vim.lsp.config(server_name, config)
+		vim.lsp.enable(server_name)
 	end
 end
 
