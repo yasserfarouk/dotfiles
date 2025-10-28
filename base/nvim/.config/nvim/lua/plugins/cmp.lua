de967a961eb957b941e3847d3ba12c5beb3aa62c
Yasser Mohammad <yasserfarouk@gmail.com>
2025-10-28 14:30:47 +0100

    fix(nvim): restore nvim-cmp configuration and LSP completion
    - Created dedicated cmp.lua plugin file with proper source configuration
    - Added nvim_lsp, copilot, buffer, and path completion sources
    - Fixed lazydev integration (group_index=0 only for Lua files)
    - Removed duplicate nvim-cmp config from lsp/init.lua
    - This fixes autocomplete and LSP workspace symbol issues

---
+++ base/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,46 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "copilot" },
+				{ name = "lazydev", group_index = 0 },
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+		})
+	end,
+}
