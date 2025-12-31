7c4fcaecadb99747574db92037c5986cb8e25113
Yasser Mohammad <yasserfarouk@gmail.com>
2025-12-31 14:43:30 +0900

    feat(nvim): add dropbar.nvim for breadcrumb navigation

--- base/nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ base/nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -90,4 +90,33 @@ return {
 			})
 		end,
 	},
+	-- Breadcrumbs/code navigation bar
+	{
+		"Bekaboo/dropbar.nvim",
+		event = "BufReadPost",
+		opts = {
+			bar = {
+				sources = function(buf, _)
+					local sources = require("dropbar.sources")
+					local utils = require("dropbar.utils")
+					if vim.bo[buf].ft == "markdown" then
+						return { sources.markdown }
+					end
+					if vim.bo[buf].buftype == "terminal" then
+						return { sources.terminal }
+					end
+					return {
+						sources.path,
+						utils.source.fallback({
+							sources.lsp,
+							sources.treesitter,
+						}),
+					}
+				end,
+			},
+		},
+		keys = {
+			{ "<leader>cb", function() require("dropbar.api").pick() end, desc = "Breadcrumb pick" },
+		},
+	},
 }
